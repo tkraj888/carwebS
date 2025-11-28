09121d667aa309ecde51457fbfa84e9354e354e1
tkraj888 <tusharck88@gmail.com>
2025-11-29 03:39:28 +0530

    ol

--- src/main/java/com/spring/jwt/config/AppConfig.java
+++ src/main/java/com/spring/jwt/config/AppConfig.java
@@ -170,7 +170,7 @@ public class AppConfig {
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
 
-                config.setAllowedOrigins(Arrays.asList("https://caryanamindia.com","https://www.caryanamindia.com", "http://localhost:5173","https://localhost/**","https://localhost:3000","http://localhost:8081","https://caryanam.netlify.app/"));
+                config.setAllowedOrigins(Arrays.asList("https://caryanamindia.com","https://www.caryanamindia.com", "http://localhost:5173","https://localhost/**","https://localhost:3000","http://localhost:8081","https://caryanam.netlify.app/","http://localhost:8080"));
                 config.setAllowedMethods(Collections.singletonList("*"));
                 config.setAllowCredentials(true);
                 config.setAllowedHeaders(Collections.singletonList("*"));
